8d271f92ce307636e8faac181336e16898a6694a
mayankfwu <suav1@protonmail.com>
2026-09-23 16:54:44 +0530

    actually added scorer.py

---
+++ scorer.py
@@ -0,0 +1,9 @@
+class scorer:
+    #TODO: attribute - score(int, starts at 0)
+    #TODO: attribute - correct_count(int, starts at 0)
+    #TODO: attribute - something to hold the ques+correct answers date
+    #TODO: attribute - something to hold the user chosen answers
+
+    #TODO: method - __init__ - takes in two data sources above, sets score and correct_count to 0
+    #TODO: method - loops through every question, compared the chosen answer vs correct answer, increments score/correct_count when they match
+
