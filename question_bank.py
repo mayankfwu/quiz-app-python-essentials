788fb792b1c65ba1e82e6a5c453d137af6ececf7
mayankfwu <suav1@protonmail.com>
2026-09-25 14:30:04 +0530

    completed question_bank.py's initial version

--- question_bank.py
+++ question_bank.py
@@ -2,28 +2,70 @@
 This module manages the collection of quiz questions i.e, storing, retrieving, editing, removing and providing filtered subsets (by topics or difficulty) which will be used by the quiz engine.
 """
 
-class Question:
-    # TODO: attribute - the question text (str)
-    # TODO: attribute - list of options (list of string)
-    # TODO: attribute - the correct answer (str or index into options)
-    # TODO: attribute - topic/category (str)
-    # TODO: attribute - difficulty level (str or int)
+import random
 
-    # TODO: method - __init__ to set all the above when a question is created
-    # TODO: method - a way to check if given ans is correct
-    # TODO: method - a way to represent the question as a dict (for saving to json)
+class Question:
+
+    def __init__(self, text, options, correct_answer, topic, difficulty):
+        self.text = text
+        self.options = options
+        self.correct_answer = correct_answer
+        self.topic = topic
+        self.difficulty = difficulty
+
+
+"""checks if the answer chosen is correct or not"""
+def is_correct(self, option_chosen):
+    return option_chosen.lower() == self.correct_answer.lower()
+
+
+"""dictionary"""
+def to_dict(self, text, option_chosen, correct_answer, topic, difficulty):
+    return {
+        'text': self.text,
+        'options': self.options,
+        'correct_answer': self.correct_answer,
+        'topic': self.topic,
+        'difficulty': self.difficulty
+    }
+
 
 class QuestionBank:
-    # TODO: attribute - the collection of question objects (dict keyed by a unique ques id)
+    def __init__(self):
+        self.questions = {}
+        self.question_count = 0
 
-    # TODO: method - __init__ (sets an empty dict to hold questions)
-    # TODO: method - add question (add_question)
-    # TODO: method - delete question (ques id)
-    # TODO: method - filter by topic (topic)
-    # TODO: method - edit question (ques id)
-    # TODO: method - filter by difficulty (difficulty)
-    # TODO: method - get_random_question (count of question/ num_questions)
-    # TODO: method - load from file
-    # TODO: method - save to file
+    def add_question(self, question_id):
+        self.question_count += 1
+        self.questions[f"q{self.question_count}"] = question_id
+        return question_id
 
+    def delete_question(self, question_id):
+        if question_id in self.questions:
+            del self.questions[question_id]
+        else:
+            print("Question not found")
 
+    def edit_question(self, question_id, new_question):
+        if question_id in self.questions:
+            self.questions[question_id] = new_question
+        else:
+            print("Question not found")
+
+    def filter_by_topic(self, topic):
+        matches=[]
+        for question_id, question in self.questions.items():
+            if question.topic == topic:
+                matches.append(question)
+        return matches
+
+    def filter_by_difficulty(self, difficulty):
+        matches = []
+        for question_id, question in self.questions.items():
+            if question.difficulty == difficulty:
+                matches.append(question)
+        return matches
+
+    def get_random_questions(self, num_questions):
+        question_list = list(self.questions.values())
+        return random.sample(question_list, num_questions)
