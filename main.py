ec71b29f6893ba05884aacb2afc6ac8552e07603
mayankfwu <suav1@protonmail.com>
2026-09-25 14:16:48 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+this is the entry point of the application
+"""
+
+from question_bank import QuestionBank
+from quiz_engine import quiz
+from scorer import scorer
+
+#TODO: function - main_menu(question_bank) - displays the menu (Admin/Student)
+#TODO: function - admin_menu(question_bank) - displays the admin options (add/edit/del)
+#TODO: function - student_menu(question_bank) - displays the student options (asks student how many questions / which topic / difficulty, then creates a quiz)
+#TODO: function - run_quiz(quiz) - call quiz engine and run the test
+#TODO: function - report(scorer) - display the result (calls report and shows no. of correct answers/ incorrect answers/ unanswered)
+
+if __name__ == "__main__":
+    # TODO: create a QuestionBank instance
+    # TODO: load existing questions from file (if any)
+    # TODO: call main_menu() to start the program
+    pass
