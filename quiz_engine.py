880443172af793fae8665e2bb2db5037c6df9e73
mayankfwu <suav1@protonmail.com>
2026-09-26 07:51:33 +0530

    completed quiz.engine

--- quiz_engine.py
+++ quiz_engine.py
@@ -1,13 +1,31 @@
-class quiz:
-    #TODO: attribute - question id and its options from question bank (dict)
-    #TODO: attribute - question id and the option chosen (dict)
-    #TODO: attribute - Time of test in minutes (int)
-    #TODO: attribute - current index
-    #TODO: attribute - start time = 0
+import time
 
-    #TODO: method - __init__ (call get_random_question to get questions with the options, correct answer, topic, difficulty and timer start with quiz and order sequential/random/topic-wise)
-    #TODO: method - timer starting with quiz and time limit as entered by user
-    #TODO: method - show question and its options according to the current index
-    #TODO: method - go_to_question_no. (let user navigate to questions directly)
-    #TODO: method - remember selection (store as dict with key as question id and option chosen)
-    #TODO: method - end quiz when user submits the test or the timer runs out
+
+class quiz:
+    def __init__(self, questions, time_limit):
+        self.questions = questions
+        self.time_limit = time_limit
+        self.current_index = 0
+        self.answers = {}
+        self.start_time = time.time()
+
+    def get_current_question(self):
+        return self.questions[self.current_index]
+
+    def go_to_question(self, question_number):
+        if question_number in range(0, len(self.questions)):
+            self.current_index = question_number
+        else:
+            print("Invalid question number")
+
+    def record_answer(self, option_chosen):
+        self.answers[self.current_index] = option_chosen
+
+    def timer(self):
+        elapsed_seconds = time.time() - self.start_time
+        return elapsed_seconds >= self.time_limit*60
+
+    def end_quiz(self):
+        return self.timer() or len(self.answers) == len(self.questions)
+
+
