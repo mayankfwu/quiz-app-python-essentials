227f45320c78a354494046cb4f71f5507166a663
mayankfwu <suav1@protonmail.com>
2026-09-26 10:15:57 +0530

    completed initial version of scorer

--- scorer.py
+++ scorer.py
@@ -1,11 +1,24 @@
 class scorer:
-    #TODO: attribute - score(int, starts at 0)
-    #TODO: attribute - correct_count(int, starts at 0)
-    #TODO: attribute - something to hold the ques+correct answers data
-    #TODO: attribute - something to hold the user chosen answers
-    #TODO: attribute - incorrect_count(int, starts at 0)
-    #TODO: attribute - unanswered(int, starts at 0)
+    def __init__(self,questions ,answers):
+        self.questions = questions
+        self.answers = answers
+        self.score = 0
+        self.correct_count = 0
+        self.incorrect_count = 0
+        self.unanswered = 0
 
-    #TODO: method - __init__ - takes in two data sources above, sets score/correct_count, incorrect_count and unanswered to 0
-    #TODO: method - score_calculator - loops through every question, compared the chosen answer vs correct answer, increments score/correct_count when they match, increment incorrect_count when they dont match and the option is not empty and increment unanswered when the option is empty
-    #TODO: method - get_report() - return correct_count, incorrect_count, unanswered
+    def score_calculator(self):
+        index = 0
+        for question in self.questions:
+            if index not in self.answers:
+                self.unanswered += 1
+            elif question.correct_answer == self.answers[index]:
+                self.correct_count += 1
+                self.score += 1
+            else:
+                self.incorrect_count += 1
+
+            index += 1
+
+    def get_report(self):
+        return self.score, self.correct_count, self.incorrect_count, self.unanswered
